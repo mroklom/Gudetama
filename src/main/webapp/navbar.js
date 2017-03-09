8438a32d3829d25b782c9482e53e2d057242e9f2
Alex Buleon <alex.buleon@gmail.com>
2017-03-09 18:58:11 +0100

    navbar and pages rework
    We can now navigate through the site
    Add JS file to launch navbar via a script
    Change all the pages so we have the base structure

---
+++ src/main/webapp/navbar.js
@@ -0,0 +1,23 @@
+document.write('\
+\
+  <nav class="navbar navbar-default" role="navigation">\
+      <div class="navbar-header">\
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">\
+    <span class="icon-bar"></span>\
+    <span class="icon-bar"></span>\
+    <span class="icon-bar"></span>\
+  </button>\
+          <a class="navbar-brand" href="index.html">Gudetama France</a>\
+      </div>\
+      <div class="navbar-collapse collapse">\
+          <ul class="nav navbar-nav navbar-left">\
+              <li><a href="episodes.html">Episodes</a></li>\
+              <li><a href="#">Un onglet</a></li>\
+          </ul>\
+          <ul class="nav navbar-nav navbar-right">\
+              <li><a href="sign_in.html">S\'inscrire</a></li>\
+              <li><a href="about.html">A propos</a></li>\
+          </ul>\
+      </div>\
+  </nav>\
+');
