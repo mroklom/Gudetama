36cb600a9de911f06e9c06c87cd5d8ec98968290
Antoine Chédin <antoine.chedin@gmail.com>
2017-03-16 13:51:23 +0100

    Fix database filling main

--- src/main/java/fr/crystalgems/gudetama/Main.java
+++ src/main/java/fr/crystalgems/gudetama/Main.java
@@ -25,15 +25,17 @@ public class Main {
         printempsCategorie.setName("Printemps 2017");
 
         Video video1 = new Video();
+        video1.setTitle("Video 1");
         video1.setCategory(noelCategory);
         video1.setLength(144);
-        video1.setPath("//etc//coucou");
+        video1.setPath("/etc//coucou");
         video1.setReleaseDate(new Date(System.currentTimeMillis()));
 
         Video video2 = new Video();
+        video2.setTitle("Video 2");
         video2.setCategory(noelCategory);
         video2.setLength(66);
-        video2.setPath("//etc//coucou2");
+        video2.setPath("/etc//coucou2");
         video2.setReleaseDate(new Date(System.currentTimeMillis()));
 
         User user1 = new User();
@@ -41,7 +43,7 @@ public class Main {
         user1.setEmail("nom@qcq");
         user1.setPassword("1230");
         user1.setAdmin(false);
-        user1.setBookmarks(new HashSet<Video>(Arrays.asList(video1)));
+        user1.setBookmarks(new HashSet<>(Arrays.asList(video1, video2)));
 
         try {
             HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
@@ -49,9 +51,11 @@ public class Main {
             HibernateUtil.getSessionFactory().getCurrentSession().save(noelCategory);
             HibernateUtil.getSessionFactory().getCurrentSession().save(automneCategorie);
             HibernateUtil.getSessionFactory().getCurrentSession().save(printempsCategorie);
-
+            HibernateUtil.getSessionFactory().getCurrentSession().save(video1);
+            HibernateUtil.getSessionFactory().getCurrentSession().save(video2);
+            HibernateUtil.getSessionFactory().getCurrentSession().save(user1);
             HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().commit();
-            System.out.println("Category saved!");
+            System.out.println("Data inserted!");
         } catch (HibernateException e) {
             HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().rollback();
             e.printStackTrace();
