5c39175c872b39ad566af5f79d16f2978c3a0437
Antoine Chédin <antoine.chedin@gmail.com>
2017-03-19 21:22:51 +0100

    New nav bar

--- src/main/webapp/navbar.js
+++ src/main/webapp/navbar.js
@@ -1,23 +1,48 @@
-document.write('\
-\
-  <nav class="navbar navbar-default" role="navigation">\
-      <div class="navbar-header">\
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">\
+if (sessionStorage.length == 0)
+    document.getElementById('navbar').innerHTML = '\
+    <div class="navbar-header">\
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">\
             <span class="icon-bar"></span>\
             <span class="icon-bar"></span>\
             <span class="icon-bar"></span>\
-          </button>\
-          <a class="navbar-brand" href="index.html">Gudetama France</a>\
-      </div>\
-      <div class="navbar-collapse collapse">\
-          <ul class="nav navbar-nav navbar-left">\
-              <li><a href="episodes_list.html">Episodes</a></li>\
-              <li><a href="about.html">A propos</a></li>\
-          </ul>\
-          <ul class="nav navbar-nav pull-right">\
-              <li><a href="sign_up.html">Inscription</a></li>\
-              <li><button type="button" class="btn btn-default navbar-btn" onclick="location.href =\'sign_in.html\'">Connexion</button></li>\
-          </ul>\
-      </div>\
-  </nav>\
-');
+        </button>\
+        <a class="navbar-brand" href="index.html">Gudetama France</a>\
+    </div>\
+    <div class="navbar-collapse collapse">\
+        <ul class="nav navbar-nav navbar-left">\
+            <li><a href="episodes_list.html">Episodes</a></li>\
+            <li><a href="about.html">A propos</a></li>\
+        </ul>\
+        <ul class="nav navbar-nav pull-right">\
+            <li><a href="sign_up.html">Inscription</a></li>\
+            <li><button type="button" class="btn btn-default navbar-btn" onclick="location.href =\'sign_in.html\'">Connexion</button></li>\
+        </ul>\
+    </div>\
+';
+else
+    document.getElementById('navbar').innerHTML = '\
+        <div class="navbar-header">\
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">\
+                <span class="icon-bar"></span>\
+                <span class="icon-bar"></span>\
+                <span class="icon-bar"></span>\
+            </button>\
+            <a class="navbar-brand" href="index.html">Gudetama France</a>\
+        </div>\
+        <div class="navbar-collapse collapse">\
+            <ul class="nav navbar-nav navbar-left">\
+                <li><a href="episodes_list.html">Episodes</a></li>\
+                <li><a href="bookmarks.html">Vidéos Favories</a></li>\
+                <li><a href="about.html">A propos</a></li>\
+            </ul>\
+            <ul class="nav navbar-nav pull-right">\
+                <li><a href="account-settings.html">Mon compte</a></li>\
+                <li><button type="button" class="btn btn-danger navbar-btn" onclick="deconnexion()">Deconnexion</button></li>\
+            </ul>\
+        </div>\
+';
+
+function deconnexion() {
+    sessionStorage.clear();
+    window.location.href = 'index.html';
+}
