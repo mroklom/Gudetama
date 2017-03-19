3e8111e937902f5ce803978e4913448223adda6a
mroklom <valentin.marechal05@gmail.com>
2017-03-19 21:23:42 +0100

    Change SubtitleService and VideoService
    add delete method

--- src/main/java/fr/crystalgems/gudetama/service/SubtitleServcie.java
+++ src/main/java/fr/crystalgems/gudetama/service/SubtitleServcie.java
@@ -1,12 +1,11 @@
 package fr.crystalgems.gudetama.service;
 
 import fr.crystalgems.gudetama.hibernate.HibernateUtil;
+import fr.crystalgems.gudetama.model.Subtitle;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * Created by Antoine on 18/03/2017.
@@ -27,5 +26,21 @@ public class SubtitleServcie {
             throw e;
         }
         return publisherName;
+    }
+
+    @PUT
+    @Path("delete")
+    @Consumes(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+    public Response deleteUser(Subtitle subtitleToDelete) {
+        try {
+            HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
+            Subtitle subtitle = HibernateUtil.getSessionFactory().getCurrentSession().load(Subtitle.class, subtitleToDelete.getId());
+            HibernateUtil.getSessionFactory().getCurrentSession().delete(subtitle);
+            HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().commit();
+        } catch (RuntimeException e) {
+            HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().rollback();
+            throw e;
+        }
+        return Response.status(200).build();
     }
 }
